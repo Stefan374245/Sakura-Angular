bfd81ac9650fe72cb5bbab9cd09030cd4a13b6de
Stefan374245 <shelldobler@gmx.de>
2025-07-17 15:47:00 +0200

    dist_file

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landingPage.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
 import { HowToOrderComponent } from './how-to-order/how-to-order.component';
+import { LocationComponent } from './location/location.component';``
 
 @Component({
   selector: 'app-main-content',
@@ -12,6 +13,7 @@ import { HowToOrderComponent } from './how-to-order/how-to-order.component';
     LandingPageComponent,
     OurMenuComponent,
     HowToOrderComponent,
+    LocationComponent,
   ],
   templateUrl: './main-content.component.html',
   styleUrl:    './main-content.component.scss'
